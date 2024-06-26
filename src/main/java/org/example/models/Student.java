3cdd51049c31bd6e68f29fe894100d8b4c5d2328
unknown <umairalvi1998@gmail.com>
2024-06-26 12:04:12 +0530

    Added Student class

--- src/main/java/org/example/models/Student.java
+++ src/main/java/org/example/models/Student.java
@@ -1,7 +1,7 @@
 package org.example.models;
-
 public class Student {
-    private String name;
-    private double psp;
-    private String batch;
+     String name;
+     Double psp;
+     int streak;
+     String email;
 }
